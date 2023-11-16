15b10d7ff2195189a9b6f6ffa96310f2aad58151
Rickard <rly@combination.se>
2023-11-16 23:24:14 +0100

    Updated to new API

--- electrolux.js
+++ electrolux.js
@@ -1,5 +1,14 @@
 'use strict';
 
+const Homey = require('homey');
+const fetch = require("node-fetch");
+
+
+/**
+ * Represents an authentication error.
+ * @class
+ * @extends Error
+ */
 class AuthError extends Error {
     constructor(message) {
         super(message);
@@ -7,126 +16,247 @@ class AuthError extends Error {
     }
 }
 
-module.exports.AuthError = AuthError;
 
-module.exports.ElectroluxDeltaApi = (function () {
-    const fetch = require("node-fetch");
-    const Homey = require('homey');
+/**
+ * Represents the Electrolux API.
+ */
+class ElectroluxApi {
+    /**
+     * Constructs a new ElectroluxApi object.
+     * @constructor
+     */
+    constructor() {
+        // Initializes the API URLs and credentials.
+        this.BASE_URL = "https://api.ocp.electrolux.one/";
+        this.TOKEN_URL = this.BASE_URL + "one-account-authorization/api/v1/token";
+        this.AUTHENTICATION_URL = this.BASE_URL + "one-account-authentication/api/v1/authenticate";
+        this.API_URL = this.BASE_URL + "appliance/api/v2/appliances";
+        this.CLIENT_ID = Homey.env.CLIENT_ID;
+        this.CLIENT_SECRET = Homey.env.CLIENT_SECRET
+        this.X_API_KEY = Homey.env.X_API_KEY;
 
-    const BASE_URI = "https://api.delta.electrolux.com/api/"
-    const CLIENT_VERSION = "1.8.16400";
-    const CLIENT_SECRET = Homey.env.WELLBEING_CLIENT_SECRET
-
-    let auth_state = {
-        clientToken: "",
-        userToken: "",
-        username: "",
-        password: ""
+        // Initializes the authentication state.
+        this.auth_state = {
+            accessToken: "",
+            idToken: "",
+            exchangeToken: "",
+            expiresAt: null,
+            username: "",
+            password: "",
+            countryCode: "",
+            failTime: null
+        };
     }
 
-    async function checkForUpdate() {
-        const response = await (await fetch(BASE_URI + "updates/Wellbeing", {
+    /**
+     * Obtains an access token using the client credentials grant type.
+     * @throws {AuthError} If an authentication error occurs.
+     * @returns {Promise<void>} A promise that resolves when the access token is obtained.
+     */
+    async getAccessToken() {
+        const tokenResponse = await fetch(this.TOKEN_URL, {
             method: "POST",
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
-                Version: CLIENT_VERSION,
-                Platform: "iOS"
-            }),
-            headers: { 'Content-Type': 'application/json' }
-        })).json()
-        if (response.forceUpdate === undefined) {
-            console.log("Invalid response from update server")
+                clientId: this.CLIENT_ID,
+                clientSecret: this.CLIENT_SECRET,
+                grantType: "client_credentials"
+            })
+        });
+
+        const tokenData = await tokenResponse.json();
+        if (!tokenData.accessToken) {
+            throw new AuthError("Error obtaining client token");
         }
-        if (response.forceUpdate) {
-            console.log("Back-end API needs to be updated")
-        }
+
+        this.auth_state.accessToken = tokenData.accessToken;
     }
 
-    async function refreshClientToken() {
-        await checkForUpdate()
-        const response = await (await fetch(BASE_URI + "Clients/Wellbeing", {
+    /**
+     * Authenticates the user by obtaining an access token and exchanging it for an idToken.
+     * @throws {AuthError} If an authentication error occurs.
+     * @returns {Promise<void>} A promise that resolves when the authentication is successful.
+     */
+    async authenticateUser() {
+        await this.getAccessToken();
+        const loginResponse = await fetch(this.AUTHENTICATION_URL, {
             method: "POST",
+            headers: { 
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${this.auth_state.accessToken}`,
+                'x-api-key': this.X_API_KEY
+            },
             body: JSON.stringify({
-                ClientSecret: CLIENT_SECRET
-            }),
-            headers: { 'Content-Type': 'application/json' }
-        })).json()
-        if (!response.accessToken) {
-            throw new AuthError("Error refreshing client token: " + response.codeDescription)
+                username: this.auth_state.username,
+                password: this.auth_state.password
+            })
+        });
+
+        const loginData = await loginResponse.json();
+        if (!loginData.idToken) {
+            throw new AuthError("Error obtaining user idToken");
         }
-        auth_state.clientToken = response.accessToken
+
+        this.auth_state.idToken = loginData.idToken;
+        this.auth_state.countryCode = loginData.countryCode;
     }
 
-    async function refreshUserToken() {
-        await refreshClientToken()
-        const response = await (await fetch(BASE_URI + "Users/Login", {
+    /**
+     * Exchanges the user's idToken for an access token using the token exchange grant type.
+     * @throws {AuthError} If an authentication error occurs.
+     * @throws {Error} If an error occurs while exchanging the token.
+     * @returns {Promise<void>} A promise that resolves when the token exchange is successful.
+     */
+    async exchangeToken() {
+        await this.authenticateUser();
+        const tokenExchangeResponse = await fetch(this.TOKEN_URL, {
             method: "POST",
+            headers: { 
+                'Content-Type': 'application/json',
+                'Origin-Country-Code': this.auth_state.countryCode
+            },
             body: JSON.stringify({
-                userName: auth_state.username,
-                password: auth_state.password
-            }),
+                clientId: this.CLIENT_ID,
+                idToken: this.auth_state.idToken,
+                grantType: "urn:ietf:params:oauth:grant-type:token-exchange"
+            })
+        });
+
+        const exchangeData = await tokenExchangeResponse.json();
+        if (!exchangeData.accessToken) {
+            throw new AuthError("Error exchanging token");
+        }
+
+        
+        this.auth_state.exchangeToken = exchangeData.accessToken;
+        this.auth_state.expiresAt = new Date(new Date().getTime() + exchangeData.expiresIn * 1000);
+    }
+
+    /**
+     * Handles an error by updating the authentication state and throwing the error.
+     * @param {Error} error - The error to handle.
+     * @throws {Error} The original error that was thrown.
+     */
+    handleError(error) {
+        this.auth_state.failTime = Date.now();
+        throw error;
+    }
+    
+    /**
+     * Fetches data from the Electrolux API.
+     * @param {string} suffix - The API endpoint to fetch data from.
+     * @param {string} method - The HTTP method to use for the request. Defaults to 'GET'.
+     * @param {Object} body - The request body to send with the request. Defaults to null.
+     * @throws {AuthError} If an authentication error occurs.
+     * @throws {Error} If an error occurs while fetching data from the API.
+     * @returns {Promise<Object>} A promise that resolves with the response data from the API.
+     */
+    async fetchApi(suffix, method = 'GET', body = null) {    
+        if (!this.auth_state.exchangeToken || new Date() >= this.auth_state.expiresAt) {
+            await this.exchangeToken();
+        }
+        
+        console.log(`fetchApi(${suffix}, ${method}, ${body})`);
+
+
+        const options = {
+            method: method,
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + auth_state.clientToken
+                'Authorization': `Bearer ${this.auth_state.exchangeToken}`,
+                'x-api-key': this.X_API_KEY
             }
-        })).json()
-        if (!response.accessToken) {
-            throw new AuthError("Login error: " + response.codeDescription)
-        }
-        auth_state.userToken = response.accessToken
+        };
+        
+        
+        if (body) {
+            options.body = JSON.stringify(body);
+         }
+
+        try {
+            const response = await fetch(this.API_URL + suffix, options);
+            const responseBody = await response.text(); // Henter responsen som tekst én gang
+
+            if (!response.ok) {
+                console.error(`API request to ${suffix} failed with status: ${response.status}`);
+                console.error(`API response body: ${responseBody}`);
+                this.handleError(new Error(`API request failed: ${response.status} ${response.statusText}`));
+                return;
+            }
+        
+            return responseBody ? JSON.parse(responseBody) : null; // Forenklet returhåndtering
+
+            } catch (error) {
+                console.error(`Error in fetchApi: ${error}`);
+                this.handleError(error);
+            }
     }
 
-    async function fetchApi(suffix, options) {
-        if (!options) options = {}
-        if (!options.headers) options.headers = {}
-        for (var i = 0; i < 3; ++i) {
-            if (auth_state.userToken) {
-                options.headers.Authorization = "Bearer " + auth_state.userToken
-            }
-            const response = await fetch(BASE_URI + suffix, options)
-            if (response.status == 200) {
-                return await response.json()
-            } else if (response.status >= 400 && response.status < 500) {
-                console.log("Error: " + response.status)
-                await refreshUserToken()
-            } else {
-                throw new Error("Internal server error: " + response.status)
-            }
-        }
-        throw new Error("Internal error. Too many authentication attempts")
-    }
-
-    class ElectroluxDeltaApi {
-        setToken(token) {
-            auth_state.userToken = token
-        }
-
-        async verifyCredentials() {
-            await refreshUserToken()
-        }
-
-        setAuth(username, password) {
-            auth_state.username = username
-            auth_state.password = password
-            auth_state.mmsToken = ""
-            auth_state.userToken = ""
-        }
-
-        async getAppliances() {
-            return await fetchApi("Domains/Appliances")
-        }
-
-        async getAppliance(pncId) {
-            return await fetchApi("Appliances/" + pncId)
-        }
-
-        async sendDeviceCommand(id, command) {
-            return await fetchApi("Appliances/" + id + "/Commands", {
-                method: "PUT",
-                body: JSON.stringify(command),
-                headers: { 'Content-Type': 'application/json' }
-            })
+    /**
+     * Sets the authentication credentials for the Electrolux API.
+     * @param {string} username - The username to use for authentication.
+     * @param {string} password - The password to use for authentication.
+     */
+    setAuth(username, password) {
+        this.auth_state.username = username;
+        this.auth_state.password = password;
+    }    
+   
+    /**
+     * Fetches all appliances.
+     * @throws {Error} If an error occurs while fetching the appliances.
+     * @returns {Promise<Object>} A promise that resolves with an array of appliance objects.
+     */
+    async getAppliances() {
+        try {
+            return await this.fetchApi('/', 'GET');
+        } catch (error) {
+            this.log(`Error fetching appliances: ${error}`);
+            throw error; // Re-throw error for further handling
         }
     }
 
-    return ElectroluxDeltaApi
-})()
+    /**
+     * Fetches an appliance with the specified PNC ID.
+     * @param {string} pncId - The PNC ID of the appliance to fetch.
+     * @throws {Error} If an error occurs while fetching the appliance.
+     * @returns {Promise<Object>} A promise that resolves with the appliance object.
+     */
+    async getAppliance(pncId) {
+        try {
+            if (!pncId) {
+                throw new Error("Missing device ID");
+            }
+            return await this.fetchApi(`/${pncId}`, 'GET');
+        } catch (error) {
+            this.log(`Error fetching appliance with ID ${pncId}: ${error}`);
+            throw error; // Re-throw error for further handling
+        }
+    }
+
+    /**
+     * Logs a message to the console.
+     * @param {string} message - The message to log.
+     */
+    log(message) {
+        console.log(message);
+    }
+
+
+    /**
+     * Sends a command to a device with the specified ID.
+     * @param {string} deviceId - The ID of the device to send the command to.
+     * @param {Object} command - The command to send to the device.
+     * @throws {Error} If an error occurs while sending the command.
+     */
+    async sendDeviceCommand(deviceId, command) {
+        const suffix = `/${deviceId}/command`;   
+        try {
+            await this.fetchApi(`${suffix}`, 'PUT', command);
+        } catch (error) {
+            this.log(`Error sending command to device ID: ${deviceId}: ${error}`);
+        }
+    }
+}
+
+module.exports = { ElectroluxApi, AuthError };
